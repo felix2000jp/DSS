13caf77bfcb947d07ff119ea05bcd69a2fd67049
felix2000jp <felix2000jp@gmail.com>
2020-12-13 18:38:37 +0000

    Mapa e Aresta

---
+++ Fase_3/src/Aresta.java
@@ -0,0 +1,66 @@
+import java.util.TreeMap;
+
+public class Aresta {
+    int inicio;
+    int destino;
+    double peso;
+
+    Aresta()
+    {
+        this.inicio = -1;
+        this.destino = -1;
+        this.peso = -1;
+    }
+
+    Aresta(Aresta a)
+    {
+        this.inicio = a.getInicio();
+        this.destino = a.getDestino();
+        this.peso = a.getPeso();
+    }
+
+    Aresta(int inicio, int destino, double peso)
+    {
+        this.inicio = inicio;
+        this.destino = destino;
+        this.peso = peso;
+    }
+
+    public int getInicio()
+    {
+        return inicio;
+    }
+
+    public int getDestino()
+    {
+        return destino;
+    }
+
+    public double getPeso()
+    {
+        return peso;
+    }
+
+    public void setInicio(int inicio)
+    {
+        this.inicio = inicio;
+    }
+
+    public void setDestino(int destino)
+    {
+        this.destino = destino;
+    }
+
+    public void setPeso(double peso)
+    {
+        this.peso = peso;
+    }
+
+    @Override
+    protected Object clone()
+    {
+        return new Aresta(this);
+    }
+
+
+}
---
+++ Fase_3/src/Mapa.java
@@ -0,0 +1,136 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Mapa
+{
+    Map<Integer , List<Aresta>> mapa;
+
+    Mapa()
+    {
+        this.mapa = new TreeMap<>();
+    }
+
+    Mapa(Mapa a)
+    {
+    this.mapa = a.getMapa();
+    }
+
+    public Mapa(Map<Integer, List<Aresta>> mapa)
+    {
+        this.mapa = mapa;
+    }
+
+    public Map<Integer, List<Aresta>> getMapa() {
+        return mapa;
+    }
+
+    public void setMapa(Map<Integer, List<Aresta>> mapa) {
+        this.mapa = mapa;
+    }
+
+    public void povoamento()
+    {
+        Aresta e_p1 = new Aresta(0, 1 , 2);
+        Aresta p1_e = new Aresta(1, 0 , 2);
+        Aresta p1_p2 = new Aresta(1, 2 , 5);
+        Aresta p1_p6 = new Aresta(1, 6 , 5);
+        Aresta p2_p1 = new Aresta(2, 1 , 5);
+        Aresta p2_p3 = new Aresta(2, 3 , 5);
+        Aresta p3_p2 = new Aresta(3, 2 , 5);
+        Aresta p3_p4 = new Aresta(3, 4 , 5);
+        Aresta p4_p3 = new Aresta(4, 3 , 5);
+        Aresta p4_p5 = new Aresta(4, 5 , 5);
+        Aresta p5_p4 = new Aresta(5, 4 , 5);
+        Aresta p5_s = new Aresta(5, 11 , 2.5);
+        Aresta p6_p1 = new Aresta(6, 1 , 5);
+        Aresta p6_p7 = new Aresta(6, 7 , 5);
+        Aresta p7_p6 = new Aresta(7, 6 , 5);
+        Aresta p7_p8 = new Aresta(7, 8 , 5);
+        Aresta p8_p7 = new Aresta(8, 7 , 5);
+        Aresta p8_p9 = new Aresta(8, 9 , 5);
+        Aresta p9_p8 = new Aresta(9, 8 , 5);
+        Aresta p9_p10 = new Aresta(9, 10 , 5);
+        Aresta p10_p9 = new Aresta(10, 9 , 5);
+        Aresta p10_s = new Aresta(10, 11 , 2.5);
+        Aresta s_p5 = new Aresta(11, 5 , 2.5);
+        Aresta s_p10 = new Aresta(11, 10 , 2.5);
+
+        List<Aresta> entrada = new ArrayList<>();
+        entrada.add(e_p1);
+
+        List<Aresta> prateleira1 = new ArrayList<>();
+        prateleira1.add(p1_e);
+        prateleira1.add(p1_p2);
+        prateleira1.add(p1_p6);
+
+        List<Aresta> prateleira2 = new ArrayList<>();
+        prateleira2.add(p2_p1);
+        prateleira2.add(p2_p3);
+
+        List<Aresta> prateleira3 = new ArrayList<>();
+        prateleira3.add(p3_p2);
+        prateleira3.add(p3_p4);
+
+        List<Aresta> prateleira4 = new ArrayList<>();
+        prateleira4.add(p4_p3);
+        prateleira4.add(p4_p5);
+
+        List<Aresta> prateleira5 = new ArrayList<>();
+        prateleira5.add(p5_p4);
+        prateleira5.add(p5_s);
+
+        List<Aresta> prateleira6 = new ArrayList<>();
+        prateleira6.add(p6_p1);
+        prateleira6.add(p6_p7);
+
+        List<Aresta> prateleira7 = new ArrayList<>();
+        prateleira7.add(p7_p6);
+        prateleira7.add(p7_p8);
+
+        List<Aresta> prateleira8 = new ArrayList<>();
+        prateleira8.add(p8_p7);
+        prateleira8.add(p8_p9);
+
+        List<Aresta> prateleira9 = new ArrayList<>();
+        prateleira9.add(p9_p8);
+        prateleira9.add(p9_p10);
+
+        List<Aresta> prateleira10 = new ArrayList<>();
+        prateleira10.add(p10_p9);
+        prateleira10.add(p10_s);
+
+        List<Aresta> saida = new ArrayList<>();
+        saida.add(s_p5);
+        saida.add(s_p10);
+
+        this.mapa.clear();
+        this.mapa.put(0, entrada);
+        this.mapa.put(1, prateleira1);
+        this.mapa.put(2, prateleira2);
+        this.mapa.put(3, prateleira3);
+        this.mapa.put(4, prateleira4);
+        this.mapa.put(5, prateleira5);
+        this.mapa.put(6, prateleira6);
+        this.mapa.put(7, prateleira7);
+        this.mapa.put(8, prateleira8);
+        this.mapa.put(9, prateleira9);
+        this.mapa.put(10, prateleira10);
+        this.mapa.put(11, saida);
+    }
+
+    public List<Integer> calculaRota(int lRobot, int lPalete, int destino)
+    {
+        List<Integer> rota = new ArrayList<>();
+
+
+        return rota;
+    }
+
+    @Override
+    protected Object clone()
+    {
+        return new Mapa(this);
+    }
+}
