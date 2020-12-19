1af14503f6ec98d45d168a1a6fc73e174ca4a71c
zebarros17 <61798839+zebarros17@users.noreply.github.com>
2020-12-19 15:23:20 +0000

    Organizacao

---
+++ out/production/dss/BaseDadosArmazem.sql
@@ -0,0 +1,13 @@
+create schema armazem;
+
+use armazem;
+
+CREATE USER  'gestor'@'localhost'
+	IDENTIFIED BY 'gestor';
+
+GRANT ALL ON armazem.* TO 'gestor'@'localhost';
+DROP TABLE paletes;
+DROP TABLE prateleiras;
+
+select * from paletes;
+select * from prateleiras;
